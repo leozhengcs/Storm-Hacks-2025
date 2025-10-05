90cc5b20f9eb0f61dbd3d2beb7d1617c57b7a89e
sxht <31171951+sxht@users.noreply.github.com>
2025-10-05 01:50:56 -0700

    styling

--- src/app/components/LineGraph.tsx
+++ src/app/components/LineGraph.tsx
@@ -48,9 +48,9 @@ export default function LineGraph() {
   ]
 
   return (
-    <div className='flex flex-col bg-card w-full h-[42vh] rounded-xl drop-shadow-sm'>
+    <div className='flex flex-col border-2 border-cardBorder bg-card w-full h-[42vh] rounded-xl drop-shadow-sm'>
       <div className='ml-4 mt-2 h-[5vh] text-[#111111]'><b>Daily Visits</b></div>
-      <div className='h-[97.5%] w-[97.5%] self-center bg-backgroundWhite rounded-xl mb-2 pt-4 pr-4'>
+      <div className='h-[97.5%] w-[97.5%] self-center border-2 border-cardBorder bg-backgroundWhite rounded-xl mb-2 pt-4 pr-4'>
         <ResponsiveContainer width="100%" height="100%">
         <LineChart width={730} height={250} data={data}
           margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
--- src/app/components/PieGraph.tsx
+++ src/app/components/PieGraph.tsx
@@ -29,9 +29,9 @@ export default function PieGraph() {
 
 
   return (
-    <div className='flex flex-col bg-card w-full h-[42vh] rounded-xl drop-shadow-sm'>
+    <div className='flex flex-col border-2 border-cardBorder bg-card w-full h-[42vh] rounded-xl drop-shadow-sm'>
       <div className='ml-4 mt-2 h-[5vh]'><b>Visit Sources</b></div>
-      <div className='h-[95%] w-[95%] self-center bg-backgroundWhite rounded-xl mb-2 pt-4 pr-4'>
+      <div className='h-[95%] w-[95%] self-center border-2 border-cardBorder bg-backgroundWhite rounded-xl mb-2 pt-4 pr-4'>
         <ResponsiveContainer width="100%" height="100%">
           <PieChart width={400} height={400}>
             <Pie
