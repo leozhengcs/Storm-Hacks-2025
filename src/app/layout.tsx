b43f9f6eb70bc164ec462b70184d3f4fdd86d2ae
Leo Zheng <leozhengcs@gmail.com>
2025-10-05 00:56:01 -0700

    Update navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import logo from "@/static/logo.png"
-import profileMain from "@/static/profileMain.png"
+import logo from "@/static/logo.png";
+import profileMain from "@/static/profileMain.png";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,20 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <nav className="flex items-center justify-between w-[100vw] h-[7vh] my-5 px-12">
-          <img className="max-h-[85%] object-contain" src={logo.src} alt="Logo" />
-
-          <div className="flex flex-row items-center gap-x-10 bg-customDarkBlue rounded-full px-7 h-full">
-            <button className="text-white">Dashboard</button>
-            <button className="text-white">Select Org</button>
-            <button className="text-white">Personas</button>
-          </div>
-
-          <img className="h-full object-cover rounded-full" src={profileMain.src} alt="Profile" />
-        </nav>
-        <div className='px-12'>
-          {children}
-        </div>
+        <Navbar />
+        <div className="px-12">{children}</div>
       </body>
     </html>
   );
